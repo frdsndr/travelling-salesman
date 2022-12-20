a3e3d718a1ac9804df1bb15b0abbd97841e732a3
Fred <107436653+frdsndr@users.noreply.github.com>
2022-12-20 14:04:14 +0000

    Add models and update model positions

--- src/lib/components/Travel/Travel.svelte
+++ src/lib/components/Travel/Travel.svelte
@@ -16,7 +16,7 @@
 	import Lights from '$lib/components/Travel/lighting/Lights.svelte';
 
 	// utilities
-	import Helpers from '$lib/components/Travel/utils/Helpers.svelte';
+	// import Helpers from '$lib/components/Travel/utils/Helpers.svelte';
 	import Background from '$lib/components/Travel/utils/Background.svelte';
 	import ShadowPlane from './utils/ShadowPlane.svelte';
 
@@ -34,25 +34,32 @@
 
 		<!-- geometry -->
 		<Cube />
+		<House position={{ x: -20, y: 0, z: -6 }} />
 		<House position={{ x: -8, y: 0, z: -6 }} />
 		<House position={{ x: 8, y: 0, z: -6 }} />
 		<House position={{ x: 16, y: 0, z: -6 }} />
+		<House position={{ x: 32, y: 0, z: -6 }} />
 		<T.Group position.x={$positionSalesman}>
 			<Salesman />
 		</T.Group>
 
 		<TreeSquare variant="l1" position={{ x: 2, y: 0, z: -7 }} />
+		<TreeSquare variant="s1" position={{ x: -15, y: 0, z: -10 }} />
 		<TreeSquare variant="s1" position={{ x: -4, y: 0, z: -4 }} />
 		<TreeSquare variant="s2" position={{ x: 12, y: 0, z: -9 }} />
+		<TreeSquare variant="l1" position={{ x: 27, y: 0, z: -8 }} />
+		<TreeSquare variant="s2" position={{ x: 24, y: 0, z: -5 }} />
 
 		<BushSquare variant="l1" position={{ x: 4, y: 0, z: 7 }} />
+		<BushSquare variant="l1" position={{ x: -17, y: 0, z: -4 }} />
 		<BushSquare variant="s1" position={{ x: 6, y: 0, z: -4 }} />
+		<BushSquare variant="s1" position={{ x: -7, y: 0, z: -4 }} />
 		<BushSquare variant="s2" position={{ x: 8, y: 0, z: -4 }} />
 
 		<Street />
 
 		<!-- utilities -->
-		<Helpers />
+		<!-- <Helpers /> -->
 		<Background color={'#739368'} />
 		<ShadowPlane />
 	</Canvas>
--- src/lib/components/Travel/models/Salesman.svelte
+++ src/lib/components/Travel/models/Salesman.svelte
@@ -2,7 +2,7 @@
 	import { GLTF } from '@threlte/extras';
 
 	export let scale = 0.01;
-	export let position = { x: 0, y: 0, z: 0 };
+	export let position = { x: 0, y: 0, z: -1.5 };
 
 	const url = '/geometry/characters/salesman.glb';
 
